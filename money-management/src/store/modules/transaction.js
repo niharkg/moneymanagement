5db247726f5ea5a4fabc39ad94187cd4ba234194
C0deZ Lee <c0dezlee@gmail.com>
2018-10-17 22:24:00 -0400

    Change API

--- money-management/src/store/modules/transaction.js
+++ money-management/src/store/modules/transaction.js
@@ -22,20 +22,28 @@ const actions = {
   getRecentTransactions({ commit }, params) {
     console.log("here")
     return transactionApi.getRecentTransactions(params.user_id, params.amount)
-    .then((response) => {
-      return (response)
-    })
-    .catch((error) => Promise.reject(error))
-  }, 
+      .then((response) => {
+        return Promise.resolve(response)
+      })
+      .catch((error) => Promise.reject(error))
+  },
   // Get all of  a user's transactions
-  getCurrentMonthCategorySpendings({ commit },params) {
+  getCurrentMonthCategorySpendings({ commit }, params) {
     console.log(params)
-    return transactionApi.getCurrentMonthCategorySpending(params.user_id, params.month, params.year)
-    .then((response) => {
-      return (response)
-    })
-    .catch((error) => Promise.reject(error))
-  }, 
+    return transactionApi.getCurrentMonthCategorySpending(params.month, params.year)
+      .then((response) => {
+        return Promise.resolve(response)
+      })
+      .catch((error) => Promise.reject(error))
+  },
+
+  getTransactoinsByPage({ commit }, page_id) {
+    return transactionApi.getTransactoinsByPage(page_id)
+      .then((response) => {
+        return Promise.resolve(response)
+      })
+      .catch((error) => Promise.reject(error))
+  },
 }
 
 const mutations = {
