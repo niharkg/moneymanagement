5868b008c89b7d3bdf5df9fd704b95246e9938c6
Tyler Blankenship <tjbloop@gmail.com>
2018-11-11 12:00:25 -0500

    Skeleton for tips links

---
+++ money-management/src/components/Util.vue
@@ -0,0 +1,244 @@
+<script>
+
+export default {
+    name: 'Util',
+    capital_one_urls: [
+        {
+        header: "CreditWise",
+        text: "Monitor your credit. For free.",
+        url: "https://creditwise.capitalone.com/home",
+        },
+        {
+        header: "Savor® Cash Back Rewards",
+        text: "Earn unlimited 4% cash back on dining and entertainment.",
+        url: "https://www.capitalone.com/credit-cards/savor-dining-rewards/",
+        },
+        {
+        header: "Expect more with 360 Money Market®<",
+        text: "Grab one of the nation's top savings rates with this fee-free, online and mobile account. ",
+        url: "https://www.capitalone.com/bank/savings-accounts/",
+        },
+        {
+        header: "See If You're Pre-Qualified",
+        text: "Are you eligible for pre-qualified credit card offers? There's no impact to your credit score to find out now.. ",
+        url: "https://www.capitalone.com/credit-cards/prequalify",
+        },
+        {
+        header: "Auto Navigator",
+        text: "Find your perfect car, and get pre-qualified for financing—all with no impact to your credit score. ",
+        url: "https://www.capitalone.com/autonavigator/auto"
+        },
+    ],
+    dining_urls: [
+        {
+        header: "",
+        text: "", 
+        url: ""
+        },
+        {
+        header: "",
+        text: "", 
+        url: ""
+        },
+        {
+        header: "",
+        text: "", 
+        url: ""
+        },{
+        header: "",
+        text: "", 
+        url: ""
+        },
+        {
+        header: "",
+        text: "", 
+        url: ""
+        }
+    ],
+    healthcare_urls: [
+        {
+        header: "",
+        text: "", 
+        url: ""
+        },
+        {
+        header: "",
+        text: "", 
+        url: ""
+        },
+        {
+        header: "",
+        text: "", 
+        url: ""
+        },{
+        header: "",
+        text: "", 
+        url: ""
+        },
+        {
+        header: "",
+        text: "", 
+        url: ""
+        }
+    ],
+    entertainment_urls: [
+        {
+        header: "",
+        text: "", 
+        url: ""
+        },
+        {
+        header: "",
+        text: "", 
+        url: ""
+        },
+        {
+        header: "",
+        text: "", 
+        url: ""
+        },{
+        header: "",
+        text: "", 
+        url: ""
+        },
+        {
+        header: "",
+        text: "", 
+        url: ""
+        }
+    ],
+    merchandise_urls: [
+        {
+        header: "",
+        text: "", 
+        url: ""
+        },
+        {
+        header: "",
+        text: "", 
+        url: ""
+        },
+        {
+        header: "",
+        text: "", 
+        url: ""
+        },{
+        header: "",
+        text: "", 
+        url: ""
+        },
+        {
+        header: "",
+        text: "", 
+        url: ""
+        }
+    ],
+    travel_urls: [
+        {
+        header: "",
+        text: "", 
+        url: ""
+        },
+        {
+        header: "",
+        text: "", 
+        url: ""
+        },
+        {
+        header: "",
+        text: "", 
+        url: ""
+        },{
+        header: "",
+        text: "", 
+        url: ""
+        },
+        {
+        header: "",
+        text: "", 
+        url: ""
+        }
+    ],
+    grocery_urls: [
+        {
+        header: "",
+        text: "", 
+        url: ""
+        },
+        {
+        header: "",
+        text: "", 
+        url: ""
+        },
+        {
+        header: "",
+        text: "", 
+        url: ""
+        },{
+        header: "",
+        text: "", 
+        url: ""
+        },
+        {
+        header: "",
+        text: "", 
+        url: ""
+        }
+    ],
+    insurance_urls: [
+        {
+        header: "",
+        text: "", 
+        url: ""
+        },
+        {
+        header: "",
+        text: "", 
+        url: ""
+        },
+        {
+        header: "",
+        text: "", 
+        url: ""
+        },{
+        header: "",
+        text: "", 
+        url: ""
+        },
+        {
+        header: "",
+        text: "", 
+        url: ""
+        }
+    ],
+    gas_automotive_urls: [
+        {
+        header: "",
+        text: "", 
+        url: ""
+        },
+        {
+        header: "",
+        text: "", 
+        url: ""
+        },
+        {
+        header: "",
+        text: "", 
+        url: ""
+        },{
+        header: "",
+        text: "", 
+        url: ""
+        },
+        {
+        header: "",
+        text: "", 
+        url: ""
+        }
+    ]
+
+  
+}
+
+</script>
